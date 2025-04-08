e5a3e6b6b0046aec218006ac543f37d3ddb48527
stefan <94132306+sm1267@users.noreply.github.com>
2025-04-08 20:39:57 +0300

    Update and rename form.java to Form.java

---
+++ lab4/Form/Form.java
@@ -0,0 +1,19 @@
+public class Form {
+    protected String color;
+
+    public Form() {
+        this.color = "necunoscut";
+    }
+
+    public Form(String color) {
+        this.color = color;
+    }
+
+    public double getArea() {
+        return 0;
+    }
+
+    public String toString() {
+        return color;
+    }
+}
