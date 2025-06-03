47e4aa6b594d0b7e21ae149fcb7fefaa29725248
stefan <94132306+sm1267@users.noreply.github.com>
2025-06-03 10:39:22 +0300

    Create NullParameterException.java

---
+++ l7/calculator/exceptions/NullParameterException.java
@@ -0,0 +1,8 @@
+package com.example.calculator.exceptions;
+
+
+public class NullParameterException extends Exception {
+    public NullParameterException(String message) {
+        super(message);
+    }
+}
